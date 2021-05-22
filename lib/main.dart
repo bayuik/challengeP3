fed227c04c37acc2da46831d35c8c2eb17d6fe8b
bayuik <bayuindrakusuma05.com>
2021-05-23 05:46:54 +0700

    add content layout

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,44 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Row(
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100.0,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  Container(
+                    color: Colors.green.shade500,
+                    width: 100.0,
+                    height: 100.0,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100.0,
+            )
+          ],
+        ),
+      ),
+      
+    );
+  }
+}
